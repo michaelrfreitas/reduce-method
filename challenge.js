d2064cefe5e9a3bdeb0574332db2c1feed28d389
Michael Freitas <michael_rfreitas@hotmail.com>
2022-08-01 21:26:22 +0000

    Challenge reduce method

---
+++ challenge.js
@@ -0,0 +1,57 @@
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: { maths: 'Harry', english: 'Joan', cad: 'Paul' },
+        results: { maths: 90, english: 75, cad: 87 },
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: { science: 'Iris', english: 'Joan', art: 'Simon' },
+        results: { science: 93, english: 73, art: 95 },
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: { science: 'Iris', maths: 'Harry', art: 'Simon' },
+        results: { science: 93, english: 88, maths: 97, art: 95 },
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: { science: 'Iris', english: 'Joan', art: 'Simon' },
+        results: { science: 93, english: 87, art: 95 },
+    }
+];
+
+
+// const biggest = students.reduce((acc, curr) => {
+//     if (!acc) {
+//         acc = curr.results.english;
+//     } if (acc < curr.results.english) {
+//         acc = curr.results.english;
+//     }
+//     return acc;
+// }, 0);
+
+// let student = students.filter(p => p.results.english === biggest);
+
+// console.log('name: ' + student[0].name + ' max: ' + biggest);
+
+const biggest = students.reduce((acc, cur) => {
+    acc = acc.max > cur.results.english ? acc: {name:cur.name, max:cur.results.english};
+    return acc;
+    }, {name: '', max: 0});
+console.log(biggest);
+
+/* Using destructuring 
+const biggest = students.reduce(({max, name}, {name, results:{english}}) => {
+    if(max < english) {
+        acc = {name:name, max: english};
+    }
+    return acc;
+    }, {name: '', max: 0});
+    
+console.log(biggest);
+*/
